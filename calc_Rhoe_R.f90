7e51fba268332ed4b4f44e395227c2c47b48f658
Fadjar Fathurrahman <fadjar.fathurrahman@gmail.com>
2017-12-13 16:29:16 +0700

    Add comment

--- calc_Rhoe_R.f90
+++ calc_Rhoe_R.f90
@@ -1,17 +1,13 @@
-!!
-!! PURPOSE:
-!!
-!!   This subroutine calculates electronic density, given `psi`
-!!   (which need not to be Kohn-Sham states) and occupation number `Focc`
-!! 
-!! AUTHOR:
-!!
-!!   Fadjar Fathurrahman
-!!
-!! MODIFY:
-!!
-!!   Global variable `Rhoe_R`
-!!
+!!>
+!!> \section{Subroutine \texttt{calc\_Rhoe\_R}}
+!!>
+!!> This subroutine calculates electronic density, given `psi`
+!!> (which need not to be Kohn-Sham states) and occupation number `Focc`
+!!> Kohn-Sham orbitals are transformed to real space and summation over
+!!> space is done in real space too.
+!!>
+!!> This subroutine modifies global variable `Rhoe_R`
+!!>
 SUBROUTINE calc_Rhoe_R( Focc, psi )
 
   USE m_realspace, ONLY : Npoints, Ns, dVol
@@ -32,14 +28,19 @@ SUBROUTINE calc_Rhoe_R( Focc, psi )
   DO ist = 1, Nstates
     DO igw = 1,Ngwx
       idxG = idx_gw2r(igw)
+!!> Copy to an array which will be transformed to real space
       psiR(idxG,ist) = psi(igw,ist)
     ENDDO 
+!!> Transform to real space using inverse FFT
     CALL fft_fftw3( psiR(:,ist), Ns(1), Ns(2), Ns(3), .TRUE. )
   ENDDO
 
+!!> Orthogonalize in real space:
   CALL z_ortho_gram_schmidt( psiR, Npoints, Npoints, Nstates )
+!!> Normalize:
   psiR(:,:) = psiR(:,:) / sqrt(dVol)
 
+!!> Finally, calculate the electronic density
   Rhoe_R(:) = 0.d0
   DO ist = 1,Nstates
     DO ip = 1,Npoints
