fb9157cc3d2a1079d49b7ea4c486bddbe540450a
Fadjar Fathurrahman <fadjar.fathurrahman@gmail.com>
2018-06-18 18:51:50 +0700

    Free idx_gw2g, test Be atom

--- src/dealloc_PWGrid.f90
+++ src/dealloc_PWGrid.f90
@@ -6,5 +6,6 @@ SUBROUTINE dealloc_PWGrid()
   IF( allocated(Gv2) ) DEALLOCATE( Gv2 )
   IF( allocated(idx_g2r) ) DEALLOCATE( idx_g2r )
   IF( allocated(idx_gw2r) ) DEALLOCATE( idx_gw2r )
+  IF( allocated(idx_gw2g) ) DEALLOCATE( idx_gw2g )
 
 END SUBROUTINE 
