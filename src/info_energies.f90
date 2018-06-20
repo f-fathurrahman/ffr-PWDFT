ad0a39f0909304b0405a608752ffe005d0fe03fe
Fadjar Fathurrahman <fadjar.fathurrahman@gmail.com>
2018-06-20 07:04:10 +0700

    Update info energies to match ffr-LFDFT output

--- src/info_energies.f90
+++ src/info_energies.f90
@@ -6,14 +6,16 @@ SUBROUTINE info_energies()
   WRITE(*,*)
   WRITE(*,*) 'Energy components:'
   WRITE(*,*)
-  WRITE(*,fmt=999) 'Kinetic  = ', E_kinetic
-  WRITE(*,fmt=999) 'Local PS = ', E_ps_loc
-  WRITE(*,fmt=999) 'NL PS    = ', E_ps_NL
-  WRITE(*,fmt=999) 'Hartree  = ', E_Hartree
-  WRITE(*,fmt=999) 'XC       = ', E_xc
-  WRITE(*,fmt=999) 'NN       = ', E_nn
-  WRITE(*,*)       '-----------------------------'
-  WRITE(*,fmt=999) 'Total    = ', E_total
+  WRITE(*,fmt=999) 'Kinetic      = ', E_kinetic
+  WRITE(*,fmt=999) 'Local PS     = ', E_ps_loc
+  WRITE(*,fmt=999) 'NL PS        = ', E_ps_NL
+  WRITE(*,fmt=999) 'Hartree      = ', E_Hartree
+  WRITE(*,fmt=999) 'XC           = ', E_xc
+  WRITE(*,*)       '---------------------------------'
+  WRITE(*,fmt=999) '! Electronic = ', E_total - E_nn
+  WRITE(*,fmt=999) 'NN           = ', E_nn
+  WRITE(*,*)       '---------------------------------'
+  WRITE(*,fmt=999) '! Total      = ', E_total
 
 
   999 FORMAT(1x,A,F18.10)
