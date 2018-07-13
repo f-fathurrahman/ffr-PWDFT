8e5130b1c22b646396102e271ddbe3f7ce681315
Fadjar Fathurrahman <fadjar.fathurrahman@gmail.com>
2018-07-13 17:11:26 +0700

    Use the same arguments for ortho_qr

--- tests/ortho_qr.f90
+++ tests/ortho_qr.f90
@@ -1,10 +1,11 @@
 !-------------------------------------------
-SUBROUTINE z_ortho_qr(X, nbasis, nstates)
+SUBROUTINE z_ortho_qr(X, ldX, nbasis, nstates)
 !-------------------------------------------
   IMPLICIT NONE
   ! Argument
   INTEGER :: nbasis, nstates
-  COMPLEX(8) :: X(nbasis,nstates)
+  INTEGER :: ldX
+  COMPLEX(8) :: X(ldX,nstates)
   ! Local
   INTEGER :: m, n, k, lwork, info
   COMPLEX(8), ALLOCATABLE :: tau(:), work(:)
@@ -18,14 +19,14 @@ SUBROUTINE z_ortho_qr(X, nbasis, nstates)
   ALLOCATE(work(lwork))
 
   ! Compute QR factorization
-  CALL zgeqrf(m,n,X,m,tau,work,lwork,info)
+  CALL zgeqrf(m,n,X,ldX,tau,work,lwork,info)
   IF(info /= 0) THEN 
     WRITE (*,'(1x,A,I4)') 'ERROR calling ZGEQRF : info = ', info
     RETURN 
   ENDIF
 
   ! Construct Q explicitly
-  CALL zungqr(m,k,k,X,m,tau,work,lwork,info)
+  CALL zungqr(m,k,k,X,ldX,tau,work,lwork,info)
   if(info /= 0) then
     WRITE(*,'(1x,A,I4)') 'ERROR calling ZUNGQR : info = ', info
     RETURN 
--- tests/test_ortho.f90
+++ tests/test_ortho.f90
@@ -1,16 +1,17 @@
 PROGRAM test_ortho
 
   IMPLICIT NONE 
-  INTEGER :: Ngwx, Nstates
+  INTEGER :: Ngwx, Nstates, Ngw
   COMPLEX(8), ALLOCATABLE :: v(:,:)
   INTEGER :: ist, igw
   REAL(8) :: r1, r2
 
-  Ngwx = 100
+  Ngwx = 1000
+  Ngw = 980
   Nstates = 4
   ALLOCATE( v(Ngwx,Nstates) )
   DO ist = 1,Nstates
-    DO igw = 1,Ngwx
+    DO igw = 1,Ngw
       CALL random_number(r1)
       CALL random_number(r2)
       v(igw,ist) = cmplx( r1, r2, kind=8 )
@@ -18,9 +19,9 @@ PROGRAM test_ortho
   ENDDO 
 
   !CALL z_ortho_gram_schmidt( v, Ngwx, Ngwx, Nstates )
-  CALL z_ortho_qr( v, Ngwx, Nstates )
+  CALL z_ortho_qr( v, Ngwx, Ngw, Nstates )
 
-  CALL z_ortho_check( Ngwx, Nstates, 1.d0, v )
+  CALL z_ortho_check( Ngw, Nstates, 1.d0, v(1:Ngw,:) )
 
   DEALLOCATE( v )
 
