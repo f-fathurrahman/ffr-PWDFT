58083fe4e5b08c3ff60fd3e7a45f8b79a34a7242
Fadjar Fathurrahman <fadjar.fathurrahman@gmail.com>
2021-05-28 17:53:28 +0700

    Print out info about evalsp and rhosp

--- src_LAPW/allatoms.f90
+++ src_LAPW/allatoms.f90
@@ -7,7 +7,7 @@ use modxcifc, only: getxcdata
   IMPLICIT NONE 
   logical :: hybrid_
   INTEGER :: xcspin_,xcgrad_
-  INTEGER :: is
+  INTEGER :: is, ist
   REAL(8) :: hybridc_
   character(512) :: xcdescr_
   ! ALLOCATABLE arrays
@@ -24,9 +24,17 @@ use modxcifc, only: getxcdata
 
   ALLOCATE(rwf(nrspmax,2,nstspmax))
   DO is=1,nspecies
+
     CALL atom(solsc,ptnucl,spzn(is),nstsp(is),nsp(:,is),lsp(:,is),ksp(:,is), &
      occsp(:,is),xctsp,xcgrad_,nrsp(is),rsp(:,is),evalsp(:,is),rhosp(:,is), &
      vrsp(:,is),rwf)
+
+    DO ist=1,nstsp(is)
+      write(*,'(1x,I3,F18.10)') ist, evalsp(ist,is)
+    ENDDO
+
+    write(*,'(1x,A,ES18.10)') 'sum(rhosp) = ', sum(rhosp)
+
   ENDDO 
   DEALLOCATE(rwf)
   
