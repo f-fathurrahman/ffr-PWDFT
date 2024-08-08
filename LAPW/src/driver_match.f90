6e95e00cbd6baff28369335af8c20450fe4dbe32
Fadjar Fathurrahman <fadjar.fathurrahman@gmail.com>
2024-08-08 19:46:18 +0700

    Loop over all spins

--- LAPW/src/driver_match.f90
+++ LAPW/src/driver_match.f90
@@ -1,4 +1,6 @@
-subroutine driver_match(jspn, ik, apwalm)
+!----------------------------------------
+subroutine driver_match(ik, apwalm)
+!----------------------------------------
   USE m_atoms, ONLY: natmtot
   USE m_gkvectors, ONLY: ngk, ngkmax, vgkc, gkc, sfacgk
   USE m_muffin_tins, ONLY: lmmaxapw
@@ -6,11 +8,25 @@ subroutine driver_match(jspn, ik, apwalm)
   USE m_spin, only: nspnfv
   implicit none
   ! argument
-  integer :: jspn, ik
+  integer :: ik
   COMPLEX(8) :: apwalm(ngkmax,apwordmax,lmmaxapw,natmtot,nspnfv)
-  
+  ! Local variables
+  integer :: jspn
+
+  write(*,*)
+  write(*,*) '>>>>>    ENTER driver_match'
+  write(*,*)
+
   ! find the matching coefficients
-  CALL match( ngk(jspn,ik), vgkc(:,:,jspn,ik), gkc(:,jspn,ik), &
-              sfacgk(:,:,jspn,ik), apwalm(:,:,:,:,jspn) )
+  do jspn = 1,nspnfv
+    CALL match( ngk(jspn,ik), vgkc(:,:,jspn,ik), gkc(:,jspn,ik), &
+                sfacgk(:,:,jspn,ik), apwalm(:,:,:,:,jspn) )
+    write(*,*) 'jspn = ', jspn, ' is done'
+  enddo
+
+  write(*,*)
+  write(*,*) '>>>>>    EXIT driver_match'
+  write(*,*)
+
   return
 end subroutine
